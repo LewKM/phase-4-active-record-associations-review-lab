3b1b9a5cc72306841c602311303d94a236f3d98c
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-09-29 17:58:55 +0300

    Done

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,6 +1,11 @@
 class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
+      t.string :name
+      t.string :description
+
+      t.belongs_to :passenger, null: false, foreign_key: true
+      t.belongs_to :taxi, null: false, foreign_key: true
 
       t.timestamps
     end
